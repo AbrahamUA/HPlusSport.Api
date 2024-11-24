2767e2d175d6782e4cb0f30e34c76c6a3f644621
AbrahamUA <thisisformypsn11@gmail.com>
2024-11-24 20:55:56 +0100

    Add files via upload

---
+++ HPlusSport/HPlusSport/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿uusing System.Text.Json.Serialization;
+
+namespace HPlusSport.Model
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Stu { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public decimal price { get; set; }
+		public bool IsAvailable { get; set; }
+
+		public int CategoryId { get; set; }
+
+		public virtual Category Category { get; set; }
+
+		[Required]
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+	}
+}
+
